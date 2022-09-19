16107352205ef148ea2f483ff395fd36b53b2291
Saravjotsingh55 <88719089+Saravjotsingh55@users.noreply.github.com>
2022-09-19 16:06:49 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,10 @@
+list1=["hello",1234,True,23.42]
+list2=["this is a string",321.44,2]
+print("list is created",list1,"\n")
+
+list1.append(list2)
+print("list2 is appended in list1: ",list1,"\n")
+
+list1.clear()
+print("list1 is clear:",list1,"\n")
+print("written by Saravjot Singh 2003249")
