c27a3bded0ceb1c93a072c1a65d2623674f53844
Saravjotsingh55 <88719089+Saravjotsingh55@users.noreply.github.com>
2022-09-19 16:10:01 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+a ="its a string"
+list1=[23,"hello","yea",23.43]
+dict={1: 'hello', 2: 'python', 3: 'world'}
+myset = set(["a", "b", "c"])
+touple1 =("this is inside touple",233)
+
+print(a,"\n",list1,"\n","set: ",myset,"\n",touple1,"\n")
+
+print("\n code written by: Saravjot Singh,2003249")
